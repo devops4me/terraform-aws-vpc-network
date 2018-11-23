34bfe68fd17116df36359fb1889fcd04969db3e8
apollo <apolloakora@gmail.com>
2018-11-23 15:47:57 +0000

    Reduced NAT gateway count in VPC network test.

--- integration.test.dir/vpc.network-test.tf
+++ integration.test.dir/vpc.network-test.tf
@@ -25,23 +25,26 @@ module just-two-subnets
     in_ecosystem           = "${local.ecosystem_id}-04"
 }
 
-module only-one-subnet
+module no-private-subnets
 {
     source                 = ".."
     in_vpc_cidr            = "10.241.0.0/16"
     in_subnets_max         =  "4"
-    in_num_private_subnets = 1
-    in_num_public_subnets  = 1
+    in_num_private_subnets = 0
+    in_num_public_subnets  = 2
+    in_create_public_gateway = false
     in_ecosystem           = "${local.ecosystem_id}-05"
 }
 
-module double-subnets
+module two-subnets-per-zone
 {
-    source                 = ".."
-    in_vpc_cidr            = "10.242.0.0/16"
-    in_num_private_subnets = 6
-    in_num_public_subnets  = 6
-    in_ecosystem           = "${local.ecosystem_id}-08"
+    source                    = ".."
+    in_vpc_cidr               = "10.242.0.0/16"
+    in_num_private_subnets    = 6
+    in_num_public_subnets     = 6
+    in_create_public_gateway  = false
+    in_create_private_gateway = false
+    in_ecosystem              = "${local.ecosystem_id}-08"
 }
 
 output subnet_ids_1{ value = "${module.subnet-count-not-stated.out_subnet_ids}" }
@@ -52,10 +55,10 @@ output subnet_ids_2{ value = "${module.just-two-subnets.out_subnet_ids}" }
 output private_subnet_ids_2{ value = "${module.just-two-subnets.out_private_subnet_ids}" }
 output public_subnet_ids_2{ value = "${module.just-two-subnets.out_public_subnet_ids}" }
 
-output subnet_ids_3{ value = "${module.only-one-subnet.out_subnet_ids}" }
-output private_subnet_ids_3{ value = "${module.only-one-subnet.out_private_subnet_ids}" }
-output public_subnet_ids_3{ value = "${module.only-one-subnet.out_public_subnet_ids}" }
+output subnet_ids_3{ value = "${module.no-private-subnets.out_subnet_ids}" }
+output private_subnet_ids_3{ value = "${module.no-private-subnets.out_private_subnet_ids}" }
+output public_subnet_ids_3{ value = "${module.no-private-subnets.out_public_subnet_ids}" }
 
-output subnet_ids_4{ value = "${module.double-subnets.out_subnet_ids}" }
-output private_subnet_ids_4{ value = "${module.double-subnets.out_private_subnet_ids}" }
-output public_subnet_ids_4{ value = "${module.double-subnets.out_public_subnet_ids}" }
+output subnet_ids_4{ value = "${module.two-subnets-per-zone.out_subnet_ids}" }
+output private_subnet_ids_4{ value = "${module.two-subnets-per-zone.out_private_subnet_ids}" }
+output public_subnet_ids_4{ value = "${module.two-subnets-per-zone.out_public_subnet_ids}" }
