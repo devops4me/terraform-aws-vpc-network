573af7258d9d7a407900b7458a3fb3eb1fe206ca
Apollo Akora <apolloakora@gmail.com>
2019-07-22 16:24:52 +0100

    updates for terraform 0.12.5

--- vpc.network-main.tf
+++ vpc.network-main.tf
@@ -167,7 +167,7 @@ resource aws_route public {
 
     route_table_id         = "${ aws_vpc.this_vpc.default_route_table_id }"
     destination_cidr_block = "0.0.0.0/0"
-    gateway_id             = "${ aws_internet_gateway.this.id }"
+    gateway_id             = "${ aws_internet_gateway.this[count.index].id }"
 }
 
 
