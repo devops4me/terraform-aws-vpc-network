47cb6d421f16476a5a60c7f5828bc7c37e10c922
Apollo Akora <apolloakora@gmail.com>
2019-07-23 09:33:30 +0100

    being cheeky and jumping the gun on array syntaces

--- vpc.network-outputs.tf
+++ vpc.network-outputs.tf
@@ -10,7 +10,8 @@
 output out_vpc_id {
 
     description = "This (string) vpc_id is the ID of the VPC that has just been created."
-    value       = "${aws_vpc.this_vpc.id}"
+    value       = aws_vpc.this_vpc.id
+############    value       = "${aws_vpc.this_vpc.id}"
 }
 
 
@@ -21,7 +22,8 @@ output out_vpc_id {
 output out_subnet_ids {
 
     description = "Every subnet ID in every availability zone of this VPC."
-    value       = concat( "${ aws_subnet.private.*.id }", "${ aws_subnet.public.*.id }" )
+    value       = concat( aws_subnet.private.*.id, aws_subnet.public.*.id )
+##########    value       = concat( "${ aws_subnet.private.*.id }", "${ aws_subnet.public.*.id }" )
 }
 
 
@@ -32,7 +34,8 @@ output out_subnet_ids {
 output out_private_subnet_ids {
 
     description = "The private subnet IDS in every availability zone of this VPC."
-    value       = "${ aws_subnet.private.*.id }"
+    value       = aws_subnet.private.*.id
+############    value       = "${ aws_subnet.private.*.id }"
 }
 
 
